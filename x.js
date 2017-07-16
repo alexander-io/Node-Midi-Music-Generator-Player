efa8b1600e6140cd7fc56de40283562fc739fe7b
alexander-io <abharris@pugetsound.edu>
2017-07-16 08:52:22 -0700

    ++handle command line args, pattern values need to be passed as parameters using the equal sign =

--- x.js
+++ x.js
@@ -54,34 +54,18 @@ const options = commandLineArgs(optionDefinitions)
 console.log('options :', options);
 
 if (options.help) {
-  console.log('Node-Midi-Music-Generator-Player Manual\n\tUsage :\n\t\t$ node x [--filename lofi.mid] [--pattern x---] [--repeat 8] [--help]\n\t\t$ node x [--filename <String>] [--pattern <String>] [--repeat <Integer>] [--help]\n\n\t\t$ node x --filename lofi_beats.mid\n\n\t\t$ node x --pattern x--- 4\n\t\t$ node x --pattern x--- --repeat 4\n\t\t\t// result : pattern = \'x---\'.repeat(4), x---x---x---x---\n\n\t\t$ node x --pattern --x- 8\n\t\t$ node x --pattern --x- --repeat 8\n\t\t\t// result : pattern = \'--x-\'.repeat(8), --x---x---x---x---x---x---x---x-')
+  console.log('Node-Midi-Music-Generator-Player Manual\n\tUsage :\n\t\t$ node x [--filename lofi.mid] [--pattern=x---] [--repeat 8] [--help]\n\t\t$ node x [--filename <String>] [--pattern=<String>] [--repeat <Integer>] [--help]\n\n\t\t$ node x --filename lofi_beats.mid\n\n\t\t$ node x --pattern=x--- 4\n\t\t$ node x --pattern=x--- --repeat 4\n\t\t\t// result : pattern = \'x---\'.repeat(4), x---x---x---x---\n\n\t\t$ node x --pattern=--x- 8\n\t\t$ node x --pattern=--x- --repeat 8\n\t\t\t// result : pattern = \'--x-\'.repeat(8), --x---x---x---x---x---x---x---x-')
   // return, do nothing
   process.exit(0)
 }
-
-
-
-// options.help ? () => {
-//   console.log('usage...')
-//   return
-// } : {}
-
-// command line arguments for nodejs are stored in process.argv
-// console.log(process.argv);
-  // or print with...
-process.argv.forEach(function(val, index, array) {
-  console.log(index + ': ' + val)
-})
-
-
 
 /*
 simple music playing/making script
 use scribbletune and wildmidi to create then play midi files
 
-first, declare a filename...
+first, declare a filename, pattern, repeat val
 */
-let filename = ''
+let filename = 'generic-music.mid', pattern = '-x--', repeat = 4
 
 // is there a filename provided in the command line options? if yes, assign filename to it, otherwise assign the default filename : 'generic-music.mid'
 options.filename ? filename = options.filename : filename = 'generic-music.mid'
@@ -89,11 +73,40 @@ options.filename ? filename = options.filename : filename = 'generic-music.mid'
 // does the file have the '.mid' entension? if no append it
 filename.includes('.mid') ? {/* pass */} : filename = filename + '.mid'
 
-console.log('filename :', filename);
+// was there a pattern provided?
+// options.pattern ? {function(){
+//   console.log('found a pattern');
+//   pattern = options.pattern[0]
+//   // XXX test print
+//   console.log('pattern :', pattern);
+//   options.pattern[1] ? repeat = options.pattern[1] : {/* pass */}
+// }} : function(){
+//   // set default pattern
+//   pattern = '-x--'
+// }
 
+// see if pattern contains x, -, or _
+let pattern_is_properly_formatted = (p) => {
+    return (p.includes('x') || p.includes('-') || p.includes('_'))
+}
+// if user provided a pattern as a command line argument, and it's properly formatted with ('x','-','_')...
+if (
+  options.pattern && pattern_is_properly_formatted(options.pattern[0])
+) /* then set pattern -> command line arg */ {
+  pattern = options.pattern[0]
+  options.pattern[1] ? repeat = options.pattern[1] : {/* pass */}
+}
+
+// XXX test print
+console.log('filename', filename);
+console.log('pattern', pattern);
+console.log('repeat', repeat);
+
+// XXX test print
+// console.log('filename :', filename);
 
 // to get a list of all the modes and scales that scribbletune can generate, you can console log scribble.scales
-// console.log('scribble scales :', scribble.scales);
+// console.log('scribble scales :', scribble.scales); // XXX test print
 
 // get a list of all the chords that scribbletune can generate, console log scribble.listChords()
 // console.log('scribble chords :', scribble.listChords());
@@ -115,7 +128,7 @@ console.log('filename :', filename);
 /* try to create a clip of the c major scale */
 let clip = scribble.clip({
   notes: scribble.scale('c', 'blues', 3), // this works too ['c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3']
-  pattern: 'x-'.repeat(8)
+  pattern: pattern.repeat(repeat)
 });
 
 // next, declare a promise to create a midi file before attempting to play the file
